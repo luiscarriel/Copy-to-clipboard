e103fd99c63550f7962db3bbeedcd865972a9291
luiscarriel <vitorluiiscarriel@gmail.com>
2022-03-28 19:39:57 -0500

    feat: function onclick

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Copy
+let result = document.getElementById('result');
+let textArea = document.getElementById("textArea");
+
+function copy(){
+    textArea.select()
+    textArea.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    
+    result.innerHTML = `${ textArea.value}`
+};
